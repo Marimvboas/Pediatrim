f7d330487f5ef9c2c97f982aa6f561b60fc4eaab
MVILLASB <mvillasb@ford.com>
2022-11-01 21:41:02 -0300

    Ajuste dialogo 1 paciente e cena do ex de sangue

--- Assets/Scripts/DialogoDoctor.cs
+++ Assets/Scripts/DialogoDoctor.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class DialogoDoctor : MonoBehaviour
-{
+{//
 	//Script Inspirado no script criado pelo canal do Youtube "Crie Seus Jogos"
 	public GameObject spechBaloon, nextBalloon;
+	public string[] dialogos1;
+	public string[] dialogos2;
 	string[] dialogos;
 	public float typingSpeed;
 	public static int iD;
 	public bool activecontrol;
 	public Text spechBaloonText;
+	public static bool blockFalaUpdate = false;
 
 	public void Start()
 	{
-		Falas(Dialogo2.dialogues);
-		activecontrol = true;
+		// Falas(dialogos1);
+		// activecontrol = true;
 	}
 
 	public void Falas(string[] txt)
@@ -37,15 +40,16 @@ public class DialogoDoctor : MonoBehaviour
 
 	public void NextSentence()
 	{
-		if (spechBaloonText.text == dialogos[iD])
+		//if (spechBaloonText.text == dialogos[iD])
 		{
 			if (iD < dialogos.Length - 1)
 			{
 				iD++;
 				spechBaloonText.text = "";
-				spechBaloon.SetActive(false);
-				nextBalloon.SetActive(nextBalloon.GetComponent<DialogoPaciente>().activecontrol);
-				nextBalloon.GetComponent<DialogoPaciente>().activecontrol = false;
+				//spechBaloon.SetActive(false);
+				//nextBalloon.SetActive(nextBalloon.GetComponent<DialogoPaciente>().activecontrol);
+				//nextBalloon.GetComponent<DialogoPaciente>().activecontrol = false;
+				StartCoroutine(TypingDialogues());
 			}
 			else
 			{
@@ -58,7 +62,15 @@ public class DialogoDoctor : MonoBehaviour
 
 	public void Update()
 	{
-		if (Input.GetMouseButtonDown(0))
+		if((Player.count==3) &&(!blockFalaUpdate)){ // Player.count conta a quantidade de sintomas acertados na linkagem
+			//dialogo final do paciente	
+
+			Falas(dialogos1);
+			activecontrol = true;
+			blockFalaUpdate = true;
+		}
+		
+		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			NextSentence();
 		}
